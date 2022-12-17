e2d612f66c486da87e18fba70d5959e83a863262
AgustinPalmaM <palma.agustin7@gmail.com>
2022-12-17 18:05:35 -0300

    Create function to filter the result div with specific brand and year selected from each select options brand and year

--- js/app.js
+++ js/app.js
@@ -1,23 +1,77 @@
 // variables
-const result = document.querySelector('#resultado');
-const year = document.querySelector('#year');
+const brandSelect = document.querySelector('#marca');
+const yearSelect = document.querySelector('#year');
+const minPriceSelect = document.querySelector('#minimo');
+const maxPriceSelect = document.querySelector('#maximo');
+const doorsSelect = document.querySelector('#puertas');
+const transmissionSelect = document.querySelector('#transmision');
+const colorSelect = document.querySelector('#color');
 const maxYear = new Date().getFullYear();
 const minYear = maxYear - 15;
+
+// Div that contains the results
+const result = document.querySelector('#resultado');
+
+// This object store data selected by the user to make a search
+const searchData = {
+  brand: '',
+  year: '',
+  maxPrice: '',
+  minPrice: '',
+  doors: '',
+  color: '',
+  transmission: '',
+}
 
 // Events
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  showCars(); // show the entire list of cars when document is loaded
+  showCars(cars); // show the entire list of cars when document is loaded
 
   fillSelectYears(); // fill the options in the years select field
 
+});
+
+brandSelect.addEventListener('change', e => {
+  searchData.brand = e.target.value;
+
+  filterCar();
+
 })
+
+yearSelect.addEventListener('change', (e) => {
+  searchData.year = e.target.value;
+  filterCar();
+})
+
+minPriceSelect.addEventListener('change', (e) => {
+  searchData.minPrice = e.target.value;
+})
+
+maxPriceSelect.addEventListener('change', (e) => {
+  searchData.maxPrice = e.target.value;
+})
+
+doorsSelect.addEventListener('change', (e) => {
+  searchData.doors = e.target.value;
+})
+
+transmissionSelect.addEventListener('change', (e) => {
+  searchData.transmission = e.target.value;
+})
+
+colorSelect.addEventListener('change', (e) => {
+  searchData.color = e.target.value;
+  console.log(searchData)
+})
+
 
 // Functions
 
-function showCars() {
+function showCars(cars) {
   
+  cleanHtml() // Delete previuos html elements to avoid duplicated lines in results
   cars.forEach(car => {
     const carHtml = document.createElement('P');
     const { brand, model, year, price, doors, color, transmission } = car;
@@ -27,6 +81,12 @@ function showCars() {
     result.appendChild(carHtml)
   })
   
+}
+
+function cleanHtml() {
+  while ( result.firstChild ) {
+    result.firstChild.remove()
+  }
 }
 
 // this function fill the options in select years field with years between a max and a min declared in variables
@@ -39,4 +99,28 @@ function fillSelectYears() {
     year.appendChild(optionYear);
   }
 
+}
+
+function filterCar() {
+  const resultFiltered = cars.filter( filterBrand ).filter( filterYear );
+  console.log(resultFiltered)
+  showCars(resultFiltered);
+}
+
+function filterBrand(car) {
+  const { brand } = searchData;
+  if ( brand ) {
+    return car.brand === brand
+  }
+
+  return car;
+}
+
+function filterYear(car) {
+  const { year } = searchData;
+  if ( year ) {
+    return car.year === parseInt(year)
+  }
+
+  return car
 }
